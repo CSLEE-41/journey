8ef27642d24feddfdec7a4517e166805f38f97d4
Chris Lee <c_leeonidas41@yahoo.com>
2015-09-16 23:43:17 -0400

    about-5-plus-hire

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,16 @@
+class AboutController < ApplicationController
+  def index
+  end
+
+  def education
+  end
+
+  def portfolio
+  end
+
+  def hire
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'about/index'
+
+  get 'about/education'
+
+  get 'about/portfolio'
+
+  get 'about/hire'
+
+  get 'about/contact'
+
   get 'welcome/index'
 
  root to: 'welcome#index'
